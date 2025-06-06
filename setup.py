2ef2cf050e3a2fe23909d88f5be0cc352ab65901
alistairgrenyer <alistair.j.grenyer@hotmail.com>
2025-06-06 23:43:10 +0100

    refactor as src architecture

--- setup.py
+++ setup.py
@@ -6,7 +6,8 @@ from setuptools import setup, find_packages
 setup(
     name="langdoc",
     version="0.1.0",
-    packages=find_packages(),
+    package_dir={"" : "src"},
+    packages=find_packages(where="src"),
     include_package_data=True,
     install_requires=[
         "click",
